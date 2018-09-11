53c84b011aa272c4992b4492a212d9da7eed79a2
Mauricio Balderrama <mbalderrama@gmail.com>
2018-09-11 18:47:12 -0400

    install: create variable for repository owner

--- install.sh
+++ install.sh
@@ -2,21 +2,22 @@
 
 # variables
 declare source_dir=~/macOS-Bootstrap
+declare gitHubUsername=MauricioBalderrama
 
 # download repository
-if [[ ! -d $source_dir ]]; then
+if [[ ! -d ${source_dir} ]]; then
     # clone
     echo ""
-    echo "${red}Downloading OSX Bootstrap...${reset} (Step: 1 of 1)"
-    git clone https://github.com/MauricioBalderrama/macOS-Bootstrap.git $source_dir
+    echo "Downloading OSX Bootstrap..."
+    git clone https://github.com/${gitHubUsername}/macOS-Bootstrap.git ${source_dir}
 else
     # update
     echo ""
-    echo "${red}Updating OSX Bootstrap...${reset} (Step: 1 of 1)"
+    echo "Updating OSX Bootstrap..."
     cd $source_dir
     git pull origin master
 fi
 
 # run
-cd $source_dir/core/
+cd ${source_dir}/core
 source run.sh
