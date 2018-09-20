12617b66162b03bf0485ae0203507aec22fd6f31
Mauricio Balderrama <mauricio@brilliance.cloud>
2018-09-20 07:37:17 -0700

    Update run.sh

--- core/run.sh
+++ core/run.sh
@@ -73,6 +73,9 @@ fi
 _i "Set Hombrew cache location: ${homebrewCache}..."
 export HOMEBREW_CACHE=${homebrewCache} # export HOMEBREW_CACHE=/Volumes/Installers/Homebrew
 
+_i "Homebrew is set to not update"
+export HOMEBREW_NO_AUTO_UPDATE=1
+
 _i "Installing Homebrew formulas..."
 brew install ${formulas[@]}
 
