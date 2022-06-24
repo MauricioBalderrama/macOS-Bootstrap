6a9c84675a8112cc84af8247fa747d18fbf809e4
Mauricio Balderrama <mbalderrama@gmail.com>
2022-06-24 08:07:03 -0400

    Update run.sh

--- core/run.sh
+++ core/run.sh
@@ -61,7 +61,6 @@ if test ! $(which brew); then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
-    exec bash -l
     exec zsh -l
 else
     _i "Updating Homebrew..."
