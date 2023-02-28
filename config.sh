cf78375f14af2316b4e0643ef6996879f8752bf7
Mauricio Balderrama <mbalderrama@gmail.com>
2023-02-28 14:54:58 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -10,7 +10,7 @@ gitHubUsername=MauricioBalderrama
 workFolder=Workspace
 
 # Set HOMEBREW_CACHE to use a different download and storage location
-homebrewCache=/Volumes/Installers/Homebrew
+homebrewCache=~/Installers/Homebrew
 
 # System configuration repository name at GitHub
 configurationGitHubRepositoryName=macOS-Hacks
